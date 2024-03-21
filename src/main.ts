9a0454d4ee3b095f47198f15ec7be03b9531fddb
Paul Genly <paul.genly.partner@decathlon.com>
2024-03-21 10:46:03 +0100

    test data injection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'test'
+		name: 'Java REF',
+		devList: [
+			{name: 'COGET', firstname: 'Guillaume'},
+			{name: 'NAKACHE', firstname: 'Clothilde'},
+			{name: 'GAAYA', firstname: 'Hassen'},
+			{name: 'GENLY', firstname: 'Paul'}
+		]
 	}
 });
 
